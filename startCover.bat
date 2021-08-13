2bcc479010d0ac7f19516a94b75a6da00f1ffac8
Tobias <urqry@student.kit.edu>
2021-08-13 17:16:21 +0200

    fixed config selection and added model loading

--- startCover.bat
+++ startCover.bat
@@ -1,3 +1,4 @@
 set "PATH=%PATH%;D:\Program Files\COVISE\zebuopt\lib;D:\Program Files\COVISE\zebuopt\bin"
 set "COCONFIG=%1"
-"D:\Program Files\COVISE\zebuopt\bin\Renderer\OpenCOVER.exe"
+echo "%COCONFIG%">> C:\\tmp\startCover-log.txt
+"D:\Program Files\COVISE\zebuopt\bin\Renderer\OpenCOVER.exe" %2
--- startCover.php
+++ startCover.php
@@ -4,12 +4,16 @@ $dir = dirname(__FILE__);
 $user = "test1";
 $pass = "qwertz123";
 $session = "1";
+$model = "C:\\tmp\\triceratops.obj";
 $config = $_GET["config"];
-$cmd = "C:\\tmp\startCover.bat \"$config\"";
+$configName = basename($config); // TODO: find out how to set full path
+$cmd = "C:\\tmp\startCover.bat $configName $model ";
 $fulcmd = "$dir\PsExec.exe -accepteula -u $user -p $pass -i $session -d \"\"$cmd\"\"";
 
 echo "run '$cmd'\n";
 //echo "run '$fulcmd'\n";
+shell_exec("copy $dir\startCover.bat C:\\tmp\startCover.bat");
+shell_exec("copy $dir\\triceratops.obj $model");
 $res = shell_exec($fulcmd);
 echo $res;
 ?>
